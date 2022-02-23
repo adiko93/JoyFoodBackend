88aa6658f1893fdb56a4e9a0ac95541df9113be5
adiko93 <adiko93@gmail.com>
2022-02-23 18:07:43 +0100

    test

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      host: `127.0.0.1:5432/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USER"),
       password: env("DATABASE_PASSWORD"),
