39cd2f03a4ac467fd1ea214cc74b4c2f44b639ed
adiko93 <adiko93@gmail.com>
2022-02-23 11:44:01 +0100

    mkdir fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,19 +1,11 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      },
+      host: `/cloudsql/${env("INSTANCE_CONNECTION_NAME")}`,
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USER"),
+      password: env("DATABASE_PASSWORD"),
     },
-    debug: false,
   },
 });
